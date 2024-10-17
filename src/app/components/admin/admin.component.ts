de93f7e790fd0dfa16f56d688934bea8236766b4
ViczeroB <vic.bar1@outlook.com>
2024-10-17 05:07:54 -0600

    cambios en admin y detalles

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-admin',
@@ -32,6 +33,28 @@ export class AdminComponent {
     nuevaContrasena: '',
     confirmarContrasena: ''
   };
+
+  alertapublicarCasa() {
+    Swal.fire({
+      title: 'Casa publicada',
+      text: 'La casa ha sido publicada exitosamente',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#1e3a5f'
+    });
+  }
+
+  alertaactualizarUsuario() {
+    Swal.fire({
+      title: 'Usuario actualizado',
+      text: 'Los datos del usuario han sido actualizados exitosamente',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      // cambiar color del boton de aceptar
+      confirmButtonColor: '#1e3a5f'
+
+    });
+  }
 
   publicarCasa() {
     console.log('Casa publicada:', this.nuevaCasa);
